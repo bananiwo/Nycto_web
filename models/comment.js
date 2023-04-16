c52a95a81e970231cd347c682179ec78fa4fcd74
Michal <bananiwo@gmail.com>
2023-04-16 17:56:13 +0200

    backend from tutorial

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+// automatically look for "Comments" collection (adds s)
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
