9a4b9ad2ec24cf120a1aae1585c2aa64e488c5f6
Michal Banasiak <bananiwo@gmail.com>
2023-06-10 21:56:17 +0200

    connect user to database

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,40 +1,48 @@
 const express = require('express')
+const User = require('../models/user');
 const router = express.Router();
 const bcrypt = require('bcrypt')
 
 router.use(express.json())
 
-const users = [{name: 'Name', password: "Password"}]
-
 router.get('/', (req, res) => {
-    res.json(users)
+    User.find()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
 router.post('/', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10) // 10 means automatically add 10-digit salt
-        const user = {name: req.body.name, password: hashedPassword }
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10) // 10 means automatically add 10-digit salt
+    const user = new User({username: req.body.username, password: hashedPassword });
+    user.save()
+        .then((result) => {
+            res.redirect('/comments')
+
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
 
 router.post('/login', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name)
-    if (user == null) {
-        return res.status(500).send('Cannot find user')
-    }
-    try {
-        if(await bcrypt.compare(req.body.password, user.password)) {
-            res.send("Logged in")
-        } else {
-            res.send("Wrong password")
+        const user = await User.findOne({username: req.body.username})
+        if (user == null) {
+            return res.status(500).send('Cannot find user')
         }
-    } catch {
-        res.status(500).send()
+        try {
+            if (await bcrypt.compare(req.body.password, user.password)) {
+                res.send("Logged in")
+            } else {
+                res.send("Wrong password")
+            }
+        } catch {
+            res.status(500).send()
+        }
     }
-})
+)
 
-module.exports = router;
+module.exports = router
