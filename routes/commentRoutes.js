0f7d6307558400c344b1d2ded2661911e069635c
Michal Banasiak <bananiwo@gmail.com>
2023-09-11 22:04:24 +0200

    comments info now display author's username

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const Comment = require('../models/comment');
+const User = require('../models/user');
 const router = express.Router();
 const auth = require('../middleware/auth')
 
 router.use(express.json())
 
-router.get('/all', (req, res) => {
-    Comment.find().sort({createdAt: -1})
-        .then((result) => {
-            res.render('comments', {title: 'All comments', comments: result})
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+router.get('/all', async (req, res) => {
+    try {
+        const sorted = Comment.find().sort({createdAt: -1})
+        // Find all comments and populate the 'owner' field with the associated user's 'username'
+        const commentsWithUsernames = await sorted.find().populate({
+            path: 'owner',
+            select: 'username -_id', // Exclude _id field from the user object
+        });
+
+        if (!commentsWithUsernames) {
+            return res.status(404).send({error: 'No comments found'});
+        }
+        res.render('comments', {title: 'All comments', comments: commentsWithUsernames})
+    } catch (e) {
+        res.status(500).send({e: 'Internal server error'})
+    }
 });
 
 router.get('/myComments', auth, async (req, res) => {
     try{
         const comments = await Comment.find({owner: req.user._id})
-        res.send(comments)
+        res.render('myComments', {comments})
+        // res.send(comments)
     } catch (e) {
         res.status(500).send()
     }
@@ -37,30 +47,23 @@ router.post('/', auth, async (req, res) => {
     } catch (e) {
         res.status(400).send()
     }
-
-    //
-    // comment.save()
-    //     .then((result) => {
-    //         res.redirect('/comments/all');
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //     })
 });
 
 router.get('/create', (req, res) => {
     res.render('create', {title: 'Create a new comment'});
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const id = req.params.id;
-    Comment.findById(id)
-        .then((result) => {
-            res.render('details', { comment: result, title: 'Comment Details' });
-        })
-        .catch((err) => {
-            res.status(404).render('404', {title: 'Comment not found'});
+    try {
+        const commentWithUsername = await Comment.findById(id).populate({
+            path: 'owner',
+            select: 'username -_id', // Exclude _id field from the user object
         });
+        res.render('details', { comment: commentWithUsername, title: 'Comment Details'})
+    } catch (e) {
+        res.status(404).render('404', {title: 'Comment not found'});
+    }
 })
 
 router.delete('/:id', auth, async (req, res) => {
@@ -73,16 +76,6 @@ router.delete('/:id', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
-
-
-    // const id = req.params.id;
-    // Comment.findByIdAndDelete(id)
-    //     .then(result => {
-    //         res.json({ redirect: '/comments' });
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     })
 })
 
 module.exports = router;
